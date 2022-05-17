6105311cf570c4a846f1ef69c99afbc8f3c0a865
Ritik Bardiyar <101331724+RitikBardiyar@users.noreply.github.com>
2022-05-17 19:21:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Ritik's ToDo Work</span> <br />
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
